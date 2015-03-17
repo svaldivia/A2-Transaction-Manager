ee852e11898bb6e22f0e89df3ee14b223d7ca0c2
Johan Henriksson <johanhenriksson@live.com>
2015-03-17 16:29:43 -0700

    objstore

---
+++ objstore.c
@@ -0,0 +1,101 @@
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include "objstore.h"
+
+void objstore_init(objstore_t** store_ptr, const char* dataFileName) 
+{
+    assert(store_ptr != NULL);
+    *store_ptr = NULL;
+
+    int fd = 0;
+    objstore_t* store = NULL;
+
+    fd = open(dataFileName, O_RDWR | O_CREAT | O_SYNC, S_IRUSR | S_IWUSR );
+    if (fd < 0) {
+        printf("Could not open data file '%s'\n", dataFileName);
+        exit(-1);
+    }
+
+    struct stat fstatus;
+    if(fstat(fd, &fstatus) < 0) {
+        printf("Filestat failed");
+        exit(-2);
+    }
+
+    if (fstatus.st_size < sizeof(objstore_t)) 
+    {
+        /* File hasn's been mapped in before so we need to make sure there is enough
+        space used in the file to hold the data.  */
+        objstore_t space;
+        memset(&space, 0, sizeof(objstore_t));
+
+        int rv = write(fd, &space, sizeof(objstore_t));
+        if (rv != sizeof(objstore_t)) {
+            printf("Some sort of writing error\n");
+            exit(-3);
+        }
+        printf("Store file created\n");
+    }
+
+    /* Map to memory */
+    store = (objstore_t*)mmap(NULL, 512, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0);
+
+    if (store == NULL) {
+        printf("Object data could not be mapped in");
+        exit(-1);
+    }
+
+    *store_ptr = store;
+}
+  
+void objstore_sync(objstore_t* store)
+{
+    assert(store != NULL);
+
+    /* Set last update time */
+    gettimeofday(&store->lastUpdateTime, NULL);
+
+    /* Flush to disc */
+    int r = msync(store, sizeof(objstore_t), MS_SYNC);
+    if (r != 0) {
+        printf("Unable to sync\n");
+        exit(-1);
+    }
+}
+
+int objstore_get_a(objstore_t* store) { return store->a; }
+void objstore_set_a(objstore_t* store, int new_a) {
+    store->a = new_a;
+}
+
+int objstore_get_b(objstore_t* store) { return store->b; }
+void objstore_set_b(objstore_t* store, int new_b) {
+    store->b = new_b;
+}
+
+const char* objstore_get_id(objstore_t* store) { return store->id_string; }
+void objstore_set_id(objstore_t* store, const char* new_id) {
+    strncpy(store->id_string, new_id, IDLEN);
+}
+
+void objstore_close(objstore_t** store_ptr) 
+{
+    assert(store_ptr != NULL);
+    objstore_t* store = *store_ptr;
+
+    int rv = munmap(store, sizeof(objstore_t));
+    if (rv < 0) {
+        printf("Unmap failed");
+        exit(-1);
+    }
+
+    *store_ptr = NULL;
+}
---
+++ objstore.h
@@ -0,0 +1,29 @@
+#ifndef OBJSTORE_H
+#define OBJSTORE_H
+
+#include "common.h"
+
+struct objstore_t 
+{
+    char id_string[IDLEN];               // This must be a null terminated ASCII string
+    struct timeval lastUpdateTime;
+    int a;
+    int b;
+    vclock_t vclock[MAX_NODES];
+};
+
+typedef struct objstore_t objstore_t;
+
+void objstore_init(objstore_t** store_ptr, const char* dataFileName);
+void objstore_close(objstore_t** store_ptr);
+void objstore_sync(objstore_t* store);
+
+int objstore_get_a(objstore_t* store);
+int objstore_get_b(objstore_t* store);
+const char* objstore_get_id(objstore_t* store);
+
+void objstore_set_a(objstore_t* store, int new_a);
+void objstore_set_b(objstore_t* store, int new_b);
+void objstore_set_id(objstore_t* store, const char* new_id);
+
+#endif
